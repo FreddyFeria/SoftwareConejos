cee336013255e54e52468870eaf1b68775694b95
FreddyFeria <32655494+FreddyFeria@users.noreply.github.com>
2018-11-29 09:30:00 -0500

    thisisjustatest

---
+++ materialDesign/material/src/app/registro/registro.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistroFormComponent } from './registro-form/registro-form.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [RegistroFormComponent]
+})
+export class RegistroModule { }
