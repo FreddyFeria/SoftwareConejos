93c5228104ba362074226bf26dee70a7c97d391b
FreddyFeria <32655494+FreddyFeria@users.noreply.github.com>
2019-05-13 10:04:11 -0500

    psss

--- materialDesign/material/src/app/shared/shared.module.ts
+++ materialDesign/material/src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { NotasComponent } from './componentes/notas/notas.component';
 import { NotasDetailComponent } from './componentes/notas/notas-detail/notas-detail.component';
+import { TableroComponent } from './componentes/tablero/tablero.component';
 
 @NgModule({
-  declarations: [NotasComponent, NotasDetailComponent],
+  declarations: [NotasComponent, NotasDetailComponent, TableroComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
