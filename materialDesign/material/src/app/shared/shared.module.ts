101cf703da99ce52bbd2521ae6ed970e9993afe1
FreddyFeria <32655494+FreddyFeria@users.noreply.github.com>
2019-06-21 20:25:48 -0500

    Revert "psss"
    This reverts commit 93c5228104ba362074226bf26dee70a7c97d391b.

--- materialDesign/material/src/app/shared/shared.module.ts
+++ materialDesign/material/src/app/shared/shared.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { SharedRoutingModule } from './shared-routing.module';
 import { NotasComponent } from './componentes/notas/notas.component';
 import { NotasDetailComponent } from './componentes/notas/notas-detail/notas-detail.component';
-import { TableroComponent } from './componentes/tablero/tablero.component';
 
 @NgModule({
-  declarations: [NotasComponent, NotasDetailComponent, TableroComponent],
+  declarations: [NotasComponent, NotasDetailComponent],
   imports: [
     CommonModule,
     SharedRoutingModule
